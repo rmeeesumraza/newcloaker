baca18dc8814ef32f0a333278b3fa9b19a90432a
rmeeesumraza <github@snobay.com>
2025-01-15 13:35:15 +0500

    Add files via upload

---
+++ embedConfig.js
@@ -0,0 +1,6 @@
+const embedConfig = {
+    defaultLink: "https://www.youtube.com/embed/dQw4w9WgXcQ", // Default link
+    alternateLink: "https://www.youtube.com/embed/3JZ_D3ELwOQ", // Alternate link
+};
+
+export default embedConfig;
---
+++ index.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Switch from "./Switch";
+import embedConfig from "./embedConfig";
+
+export default function Home() {
+    const [embedLink, setEmbedLink] = useState(embedConfig.defaultLink);
+    const [isSwitchOn, setIsSwitchOn] = useState(false);
+
+    const handleToggle = (newState) => {
+        setIsSwitchOn(newState);
+        setEmbedLink(newState ? embedConfig.alternateLink : embedConfig.defaultLink);
+    };
+
+    return (
+        <div style={{ textAlign: "center", padding: "20px" }}>
+            <h1>Embed Link Switcher</h1>
+            <iframe
+                src={embedLink}
+                width="600"
+                height="400"
+                style={{ border: "none" }}
+                title="Embed"
+            ></iframe>
+            <Switch isSwitchOn={isSwitchOn} onToggle={handleToggle} />
+        </div>
+    );
+}
