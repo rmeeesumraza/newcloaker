0bab41b343ca40a1abdea1a28988cbf63bbcb228
rmeeesumraza <github@snobay.com>
2025-01-15 13:30:39 +0500

    Delete config directory

--- config/embedConfig.js
+++
@@ -1,7 +0,0 @@
-const embedConfig = {
-    defaultLink: "https://www.youtube.com/embed/dQw4w9WgXcQ", // Default embed link
-    alternateLink: "https://www.youtube.com/embed/3JZ_D3ELwOQ", // Alternate embed link
-  };
-  
-  export default embedConfig;
-  
